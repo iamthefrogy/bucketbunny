a9d5b8880375067cc8932d117187b0afdfd3f640
Chintan Gurjar <chintangurjar@outlook.com>
2021-02-27 09:32:59 +0000

    Update bucketbunny.sh

--- bucketbunny.sh
+++ bucketbunny.sh
@@ -33,50 +33,49 @@ echo -e "
 "
 bucket=$(aws s3 ls s3://$1 --recursive --summarize 2>&1)
 
+# Listing bucket data - Readable bucket
+echo -e "\e[92mChecking if bucket is readable or not... \e[0m"
 if echo "$bucket" | grep -q "Total Objects"; then
         echo -e "\e[92mBucket found \e[0m"
         echo -e "\e[92mListing bucket data... \e[0m"
         echo "$bucket"
-        echo "Frogy_was_here" >> frogy.txt
+        else
+                 if echo "$bucket" | grep -q "NoSuchBucket"; then
+                        echo -e "\e[92mBucket does not exist. \e[0m"
+                        exit 0
+                else
+                        if echo "$bucket" | grep -q "AccessDenied"; then
+                                echo -e "\e[92mBucket is not readable. \e[0m"
+                        else
+                        echo -e "\e[92mUnknown error...Contact Author @iamthefrogy.\e[0m"
+                        exit 0
+                        fi
+                fi
+fi
 
-        ######################################## Uploading frogy.txt file on the bucket ########################################
-
-        echo -e "\e[92mUploading frogy.txt to the bucket... \e[0m"
-        aws s3 cp frogy.txt s3://$1 &>/dev/null
-        upload=$(aws s3 ls s3://$1 --recursive --summarize 2>&1)
-
-                if echo "$upload" | grep -q "frogy.txt"; then
-                        echo -e "\e[92mFile uploaded to the bucket successfully - Writable bucket \e[0m"
-                        echo -e "\e[92mListing bucket data... \e[0m"
-                        echo "$upload"
-
-        ######################################## Deleting frogy.txt file on the bucket ########################################
-
-                        echo -e "\e[92mDeleting frogy.txt to the bucket... \e[0m"
+# Uploading file to bucket
+echo -e "\e[92mChecking if bucket is writable or not... \e[0m"
+echo "Frogy_was_here" >> frogy.txt
+echo -e "\e[92mUploading frogy.txt to the bucket... \e[0m"
+aws s3 cp frogy.txt s3://$1 &>/dev/null
+upload=$(aws s3 ls s3://$1 --recursive --summarize 2>&1)
+if echo "$upload" | grep -q "frogy.txt"; then
+        echo -e "\e[92mFile uploaded to the bucket successfully - Writable bucket \e[0m"
+        echo -e "\e[92mListing bucket data... \e[0m"
+        echo "$upload"
+        echo -e "\e[92mDeleting frogy.txt to the bucket... \e[0m"
                         aws s3 rm s3://$1/frogy.txt &>/dev/null
                         echo -e "\e[92mFile deleted from the bucket successfully - Writable bucket \e[0m"
                         delete=$(aws s3 ls s3://$1 --recursive --summarize 2>&1)
                         echo -e "\e[92mListing bucket data... \e[0m"
                         echo "$delete"
                         rm frogy.txt
-        else
-                        if aws s3 cp frogy.txt s3://$1 2>&1 | grep -q "AccessDenied"; then
+else
+         if aws s3 cp frogy.txt s3://$1 2>&1 | grep -q "AccessDenied"; then
                         echo -e "\e[92mFile cannot be uploaded to the bucket - Bucket is readable but not writable... \e[0m"
                         rm frogy.txt
                         else
-                        echo -e "\e[91mUnknown error...Contact Author @iamthefrogy.  \e[0m"
+                        echo -e "\e[91mUnknown error...Contact Author @iamthefrogy.\e[0m"
                         rm frogy.txt
-                        fi
-        fi
-
-else
-                if echo "$bucket" | grep -q "NoSuchBucket"; then
-                        echo -e "\e[92mBucket does not exist. \e[0m"
-                else
-                        if echo "$bucket" | grep -q "AccessDenied"; then
-                                echo -e "\e[92mBucket is not readable. \e[0m"
-                        else
-                        echo "Unknown error"
-                        fi
-                fi
+         fi
 fi
